e985602bea554ca1d67baf05b12edd9dd424aa56
杨纪威 <875186719@qq.com>
2020-06-22 15:30:12 +0800

    修改

---
+++ src/main/java/com/atguigu/jiafa1205/AbcApp.java
@@ -0,0 +1,9 @@
+package com.atguigu.jiafa1205;
+
+/**
+ * @AUthor wei_ge
+ * @Data 2020/6/22 15:24
+ * @Version 2.0
+ **/
+public class AbcApp {
+}
---
+++ src/main/java/com/atguigu/jiafa1205/DugujiujianApp.java
@@ -0,0 +1,13 @@
+package com.atguigu.jiafa1205;
+
+/**
+ * @AUthor wei_ge
+ * @Data 2020/6/22 14:32
+ * @Version 2.0
+ **/
+public class DugujiujianApp {
+
+    public static void main(String[] args) {
+        System.out.println("hello dugujiujian....________________");
+    }
+}
