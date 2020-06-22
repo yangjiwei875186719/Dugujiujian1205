29d5d6edad13814b1a367c10d6d29f2258c8e30b
yangjiwei875186719 <875186719@qq.com>
2020-06-22 16:02:58 +0800

    new jianfa

---
+++ src/main/java/com/atguigu/jiafa1205/Newjianfa.java
@@ -0,0 +1,13 @@
+package com.atguigu.jiafa1205;
+
+/**
+ * @AUthor wei_ge
+ * @Data 2020/6/22 16:00
+ * @Version 2.0
+ **/
+public class Newjianfa {
+    public static void main(String[] args) {
+        System.out.println("新剑法");
+    }
+
+}
